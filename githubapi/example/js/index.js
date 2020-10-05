57bdc51fc80cfd1c28468b6e24d85a378fdeef03
vinckr <vincent@ory.sh>
2020-10-05 14:05:02 +0200

    feat: javascript github action
    Almost there.

--- githubapi/example/js/index.js
+++ githubapi/example/js/index.js
@@ -2,9 +2,11 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { Octokit } = require("@octokit/rest");
 const { request } = require("@octokit/request");
+var fs = require('fs');
+
 
 const octokit = new Octokit({
-    auth: "389a2376d9f0ad3c38a5a3078def2eaf6f7f0ab9 ",
+    auth: "access_token ", // you need to put a valid personal access token here
     userAgent: 'actions test',
     timeZone: 'Europe/Amsterdam',
     baseUrl: 'https://api.github.com',
@@ -22,19 +24,23 @@ const octokit = new Octokit({
       }
     })
 
-// ;(async () => {
-//async function getContributors() {}
-
 async function getContributors() {
   console.log('calling');
   owner = "ory";
   repo = "kratos";
-  const TestData = await octokit.repos.listContributors( owner, repo);  
+  anon = "true"
+  per_page = "500";
+  page = "0"
+  const TestData = await octokit.repos.listContributors({owner, repo});  
 
-  console.log(TestData);
+  console.log(TestData); //This will display the fetched information in console
+  fs.writeFile('contributors.json', 'TestData', function (err) {  // this should write the data to contributors.json, but somehow its not happening...
+    if (err) throw err;
+    console.log('Saved!');
+  });
 }
-// })()
 
 console.log('starting getting data');
-console.log(getContributors());  
+getContributors();
 console.log('end getting data');
+
